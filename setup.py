3e1603140bfcb80af6ab0fd298d6cfc55a442d17
Code-Sploit <64798467+Code-Sploit@users.noreply.github.com>
2020-05-22 12:22:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ def install():
     shortCutFileError = ''
     requiredModulesError = ''
     noErrors = ''
+    backupError = ''
     print(colors.CYAN + "Installing log file..." + colors.RESET)
     if (os.path.isfile("log/log.txt")):
         logFileError = True
@@ -32,6 +33,15 @@ def install():
         os.system("touch log/history.txt")
         noErrors = True
     print(colors.LIGHTYELLOW_EX + "Done installing log file..." + colors.RESET)
+    print(colors.LIGHTYELLOW_EX + "Moving src out of backup..." + colors.RESET)
+    if (os.path.isfile("lib/backup/src.zip") == True):
+        os.system("mv lib/backup/src.zip src/")
+        os.system("unzip src/src.zip")
+        os.system("rm src/src.zip")
+        os.system("mv src/iptracker src/iptracker.py")
+        os.system("mv lib/backup/iptracker src/iptracker.py")
+    else:
+        print(colors.LIGHTYELLOW_EX + "Warning: Could not find the backup of main-src file")
     print(colors.CYAN + "Generating shortcut execution file..." + colors.RESET)
     if (os.path.isfile("tracker.py")):
         shortCutFileError = True
@@ -70,31 +80,76 @@ def install():
 def remove():
     import os
     import os.path
-    print("Removing log file...")
-    os.system("rm log/log.txt")
-    print("Done removing log file...")
-    print("Removing history file...")
-    os.system("rm log/history.txt")
-    print("Done removing log file...")
-    print("Degenerating shortcut execution file...")
-    os.system("rm tracker.py")
-    print("Done degenerating shortcut execution file...")
-    print("Removing required modules...")
+    logFileError = ''
+    historyFileError = ''
+    shortCutFileError = ''
+    requiredModulesError = ''
+    noErrors = ''
+    backupError = ''
+    print(colors.CYAN + "Removing log file..." + colors.RESET)
+    if (os.path.isfile("log/log.txt") == False):
+        noErrors = False
+        print(colors.RED + "Error: Log file don't exists!" + colors.RESET)
+        logFileError = True
+    else:
+        os.system("rm log/log.txt")
+        noErrors = True
+    print(colors.LIGHTYELLOW_EX + "Done Removing log file..." + colors.RESET)
+    print(colors.CYAN + "Removing history file..." + colors.RESET)
+    if (os.path.isfile("log/history.txt") == False):
+        historyFileError = True
+        print(colors.RED + "Error: History file don't exists!" + colors.RESET)
+        noErrors = False
+    else:
+        os.system("rm log/history.txt")
+        noErrors = True
+    print(colors.LIGHTYELLOW_EX + "Done removing log file..." + colors.RESET)
+    print(colors.CYAN + "Degenerating shortcut execution file..." + colors.RESET)
+    if (os.path.isfile("tracker.py") == False):
+        shortCutFileError = True
+        print(colors.RED + "Shortcut file don't exists!" + colors.RESET)
+        noErrors = False
+    else:
+        os.system("rm tracker.py")
+    print(colors.LIGHTYELLOW_EX + "Done degenerating shortcut execution file..." + colors.RESET)
+    print(colors.LIGHTYELLOW_EX + "Creating backup of src script..." + colors.RESET)
+    if (os.path.isfile("src/iptracker.py") == True):
+        os.system("mv src/iptracker.py lib/backup/iptracker")
+        os.system("zip lib/backup/src.zip lib/backup/iptracker")
+        os.system("rm lib/backup/iptracker")
+        noErrors = True
+    else:
+        backupError = True
+        print(colors.LIGHTRED_EX + "E: main-src-script does'nt exists!")
+        print(colors.LIGHTRED_EX + "E: did you remove or backup it?")
+        print(colors.LIGHTRED_EX + "E: if you did then do 'python setup.py -a install'!")
+    print(colors.LIGHTYELLOW_EX + "Done installing backup of src script..." + colors.RESET)
+    print(colors.CYAN + "Removing required modules..." + colors.RESET)
     if (os.path.isfile("/usr/bin/pip") or os.path.isfile("/usr/bin/pip2") or os.path.isfile("/usr/bin/pip2.6") or os.path.isfile("/usr/bin/pip2.7")):
         os.system("pip uninstall -r lib/requirements.txt")
-        print("Done Removing required modules...")
+        print(colors.LIGHTYELLOW_EX + "Done removing required modules..." + colors.RESET)
+        noErrors = True
     else:
-        print("Failed to Remove required modules...")
-        ftirm = True
-        print("Is pip2 installed")
+        print(colors.LIGHTRED_EX + "Failed to remove required modules..." + colors.RESET)
+        requiredModulesError = True
+        noErrors = False
+        print(colors.LIGHTRED_EX + "Is pip2 installed" + colors.RESET)
+        print(colors.LIGHTYELLOW_EX + "Done removing required modules..." + colors.RESET)
     
-    print("Done Removing")
-    if (ftirm == True):
-        print("There were errors processing the removal of the required modules...")
-        print("The modules may still be installed!")
-        exit(1)
-    else:
-        exit(0)
+    print(colors.LIGHTYELLOW_EX + "Done removing" + colors.RESET)
+    print("\n\n"  + colors.LIGHTRED_EX + "Errors: ")
+    if (logFileError == True):
+        print("\n   " + colors.LIGHTRED_EX + "logFileError returned error code (1). Desc: Log file don't exists!")
+    if (historyFileError == True):
+        print("\n   " + colors.LIGHTRED_EX + "historyFileError returned error code (1). Desc: History file don't exists!")
+    if (shortCutFileError == True):
+        print("\n   " + colors.LIGHTRED_EX + "shortCutFileError returned error code (1). Desc: Shortcut don't exists!")
+    if (backupError == True):
+        print("\n   " + colors.LIGHTRED_EX + "backupError returned error code (1). Desc: main-src-file don't exists!")
+    if (requiredModulesError == True):
+        print("\n   " + colors.LIGHTRED_EX + "requiredModulesError returned error code (1). Desc: Could not remove required modules because of pip version!\n\n")
+    if (noErrors == True):
+        print("\n   " + colors.GREEN + "None!")
 
 
 def clearHistory():
